2fb1b356088201554ff13bc73d459bc354e340ce
saberlion-s <1366743948y@gmail.com>
2026-06-05 21:28:56 +0800

    Split GitHub login styles

--- theme/Yneko-Reimu/inc/github-login.php
+++ theme/Yneko-Reimu/inc/github-login.php
@@ -5,6 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 require_once YNEKO_REIMU_DIR . '/inc/github-login/settings.php';
 require_once YNEKO_REIMU_DIR . '/inc/github-login/rendering.php';
+require_once YNEKO_REIMU_DIR . '/inc/github-login/styles.php';
 require_once YNEKO_REIMU_DIR . '/inc/github-login/oauth.php';
 require_once YNEKO_REIMU_DIR . '/inc/github-login/users.php';
 require_once YNEKO_REIMU_DIR . '/inc/github-login/avatars.php';
